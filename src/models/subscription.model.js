720c00f5c93d464734e0271e43a6ef9f1f9ffc57
Sushanta Mahato <aryasushanta@gmail.com>
2024-04-20 13:06:42 +0530

    Feature: Add subscribers model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // the subscriber
+        ref:"User"
+    },
+    subscriber: {
+        type: Schema.Types.ObjectId, // the channel the subscriber is subscribing
+        ref:"User"
+    }
+    
+}, {timestamps:true})
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
